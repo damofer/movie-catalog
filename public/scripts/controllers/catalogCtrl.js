9a14334d4f6ab87e7fac33529c005a5ce9cfc5b6
David Mogollon <dmogollon@uninorte.edu.co>
2017-08-22 17:44:17 -0500

    made some mess with the keypressing event handling,

--- public/scripts/controllers/catalogCtrl.js
+++ public/scripts/controllers/catalogCtrl.js
@@ -2,7 +2,14 @@
   'use strict';
   angular.module('app')
 
-.controller('catalogCtrl',function($scope,dataService,$location,$window){
+.controller('catalogCtrl',function($scope,dataService,$window){
+
+ 
+ 
+
+
+
+
 	/*$scope.user =undefined;
 	$scope.isLoggedIn = function(){
 
@@ -20,7 +27,7 @@
 	$scope.isLoggedIn();*/
 
 	$scope.currentMovie;
-
+	$scope.listElement = $(".catalog__list__item")[0];
 	$scope.movieList = [];
 
 	$scope.getAllMovies = function(){
@@ -28,24 +35,54 @@
 
 		dataService.getAllMovies(function(response){	
 			
-		 	// if(response.data.id){
-		 	// 	if(window.location !="/#!/dashboard")
-		 	// 		window.location = "/#!/dashboard";
-		 	// 	$scope.user = response.data;
-		 	// }
-		 	console.log(response.data.entries);
+		  	console.log(response.data.entries);
 		 	$scope.movieList =response.data.entries;
+		 	$scope.setCurrentMovie($scope.movieList[0]);
 		 });
 	}
 
 	$scope.getAllMovies();
 
-	$scope.setCurrentMovie= function(movie){
+	$scope.setCurrentMovie= (movie,listElement = document.querySelector(".catalog__list__item"))=>{
+		
+		$(listElement).css({"transform":"scale(1.1)"});
+		console.log("the winner is ",$(listElement));
+		if(movie != $scope.currentMovie){
+
+		
 		$scope.currentMovie = movie;
-		console.log(movie.contents[0].url);
+		$scope.listElement = $(listElement);
+		console.log($scope.currentMovie);
 		swap(movie.contents[0].url)
+
+
+		}
 	}
 
+
+	 $( document ).keyup(function(e){
+	 	var i = $scope.movieList.indexOf($scope.currentMovie);
+	 	console.log(i);
+		  	  e = e || window.event;
+		  	    if (e.keyCode == '37') {
+			       // left arrow
+			       console.log("izquierda");
+			       if(i>0){
+			       	console.log($($scope.listElement).prev());
+			       	$($scope.listElement).css({"transform":"scale(1)"});
+			       	$scope.setCurrentMovie($scope.movieList[i-1],$($scope.listElement).prev())
+			       }
+			    }
+			    else if (e.keyCode == '39') {
+			       // right arrow
+			         console.log("derecha");
+			       if(i< $scope.movieList.length -1){
+			       	console.log($($scope.listElement).next());
+			       		$($scope.listElement).css({"display":"scale(1)"});
+			       	$scope.setCurrentMovie($scope.movieList[i+1],$($scope.listElement).next())
+			       }
+			    }
+		  });
 	function swap(newUrl) {
 		var video = document.getElementsByTagName('video')[0];
 		video.src = newUrl;
