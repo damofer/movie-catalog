d660dedf83e72753223ce384bc921e16663f6072
David Mogollon <dmogollon@uninorte.edu.co>
2017-08-23 17:19:33 -0500

    Fixed some mess with the scrolling and arrow key handling

--- public/scripts/controllers/catalogCtrl.js
+++ public/scripts/controllers/catalogCtrl.js
@@ -4,91 +4,160 @@
 
 .controller('catalogCtrl',function($scope,dataService,$window){
 
- 
- 
 
-
-
-
-	/*$scope.user =undefined;
-	$scope.isLoggedIn = function(){
-
-
-		dataService.isLoggedIn(function(response){	
-			
-		 	if(response.data.id){
-		 		if(window.location !="/#!/dashboard")
-		 			window.location = "/#!/dashboard";
-		 		$scope.user = response.data;
-		 	}
-
-		 });
-	}
-	$scope.isLoggedIn();*/
-
-	$scope.currentMovie;
-	$scope.listElement = $(".catalog__list__item")[0];
+$scope.init = function(){
+	/*Variable declaration / initialization section*/
+	$scope.currentMovie ;
+	$scope.listElement; 
 	$scope.movieList = [];
 
+	/*End of Variable declaration / initialization section*/
+
+
+	/*function initialization section*/
+
+
+ $scope.getAllMovies();
+
+ // start when document is ready;
+ angular.element(document).ready(function () {
+ 	
+ 	window.addEventListener("keydown",function(e){
+	 	e = e || window.event;
+	 	if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+   		 }
+
+   		if($scope.currentMovie == undefined){
+   			$scope.listElement =$('.catalog__list__item')[0];
+			console.log($scope.listElement);
+			$scope.setCurrentMovie($scope.movieList[0],$scope.listElement);
+   		}else{
+	 	
+	 	
+		  	  
+		  	    if (e.keyCode == '37') {
+		  	   		
+			       // left arrow   
+			       var element = $($scope.listElement).prev();
+			        if(element.length>0 ){	
+		  	   	    var movie = JSON.parse($(element).attr('ng-value'));	
+
+		  	   		// console.log(element,movie)			      	
+			       	$scope.setCurrentMovie(movie,element)
+			       }
+			    }
+			    else if (e.keyCode == '39') {			    	
+			       // right arrow	
+			        var element = $($scope.listElement).next();
+			        if(element.length>0 ){	
+		  	   		var movie = JSON.parse($(element).attr('ng-value'));
+
+		  	   		// console.log(element,movie)			      	
+			       	$scope.setCurrentMovie(movie,element)
+			        }
+			    }
+			}
+	});
+
+ 	window.addEventListener("click",function(e){
+ 		if( $(e.target.parentNode).attr('class') === $('.catalog__list__item').attr('class') ){
+ 			var listItem = e.target.parentNode;	
+ 			var movie = JSON.parse($(e.target.parentNode).attr('ng-value'));
+ 			$scope.setCurrentMovie(movie,listItem);
+ 		}
+ 		
+ 	})
+	
+
+ });
+	
+	
+
+/*End of function initialization section*/
+}
+
+	/*function definition section*/
 	$scope.getAllMovies = function(){
 
 
 		dataService.getAllMovies(function(response){	
 			
-		  	console.log(response.data.entries);
+		  	// console.log(response.data.entries);
 		 	$scope.movieList =response.data.entries;
-		 	$scope.setCurrentMovie($scope.movieList[0]);
+		 	
+		 	
 		 });
 	}
 
-	$scope.getAllMovies();
+	
 
-	$scope.setCurrentMovie= (movie,listElement = document.querySelector(".catalog__list__item"))=>{
+	$scope.setCurrentMovie= (movie,listElement )=>{
 		
+		$('.catalog__list__item').css({"transform":"scale(1)"});
 		$(listElement).css({"transform":"scale(1.1)"});
-		console.log("the winner is ",$(listElement));
-		if(movie != $scope.currentMovie){
-
 		
-		$scope.currentMovie = movie;
-		$scope.listElement = $(listElement);
-		console.log($scope.currentMovie);
-		swap(movie.contents[0].url)
+		if($scope.currentMovie != undefined){
 
+		  if(movie.title != $scope.currentMovie.title){
+		  	// console.log(movie.title , $scope.currentMovie.title)
+		  	$scope.$apply(function () {
+            $scope.currentMovie = movie;	
+        	});
+           $scope.currentMovie = movie;		
+		   $scope.listElement = $(listElement)[0]; 
 
+		    if ( $scope.listElement.offsetLeft >    ($scope.listElement.parentNode.clientWidth-$scope.listElement.clientWidth )/2) { //case when the item offset + its width  exceeds the screen width 
+		         $( $scope.listElement.parentNode).animate( { scrollLeft: $scope.listElement.offsetLeft - ($scope.listElement.parentNode.clientWidth-$scope.listElement.clientWidth)/2},500);
+		       
+		         console.log("element offset = "+$scope.listElement.offsetLeft, "element width =" +$scope.listElement.clientWidth, "windows width ="+ $scope.listElement.parentNode.clientWidth)
+		        $scope.listElement.parentNode.scrollLeft =  $scope.listElement.offsetLeft - $scope.listElement.clientWidth ;
+		       } else {
+		         $scope.listElement.parentNode.scrollLeft = 0;
+		       }
+
+			try{
+				$scope.swap($scope.currentMovie.contents[0].url);
+			}
+			catch(e){
+				console.log(e.text)
+			}
+		 }
+
+		}else{
+			
+			$scope.$apply(function () {
+            $scope.currentMovie = movie;	
+        	});	
+		     $scope.listElement = $(listElement)[0];
+		     try{
+			$scope.swap($scope.currentMovie.contents[0].url);
+			}
+			catch(e){
+				console.log(e.text)
+			}
 		}
 	}
 
 
-	 $( document ).keyup(function(e){
-	 	var i = $scope.movieList.indexOf($scope.currentMovie);
-	 	console.log(i);
-		  	  e = e || window.event;
-		  	    if (e.keyCode == '37') {
-			       // left arrow
-			       console.log("izquierda");
-			       if(i>0){
-			       	console.log($($scope.listElement).prev());
-			       	$($scope.listElement).css({"transform":"scale(1)"});
-			       	$scope.setCurrentMovie($scope.movieList[i-1],$($scope.listElement).prev())
-			       }
-			    }
-			    else if (e.keyCode == '39') {
-			       // right arrow
-			         console.log("derecha");
-			       if(i< $scope.movieList.length -1){
-			       	console.log($($scope.listElement).next());
-			       		$($scope.listElement).css({"display":"scale(1)"});
-			       	$scope.setCurrentMovie($scope.movieList[i+1],$($scope.listElement).next())
-			       }
-			    }
-		  });
-	function swap(newUrl) {
+	
+	$scope.swap = function(newUrl) {
 		var video = document.getElementsByTagName('video')[0];
 		video.src = newUrl;
 		video.load();
 		video.play();
 	}
+
+/*End of function definition section*/
+
+
+
+
+
+
+
+$scope.init();
+
 });
 
 })();
