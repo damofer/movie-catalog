0fc333f0e956e140b2b75e0939f25d891ed698b0
David Mogollon <dmogollon@uninorte.edu.co>
2017-08-24 17:37:38 -0500

    Config basic routing

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -9,6 +9,12 @@ app.config(function($routeProvider) {
             controller: 'loginCtrl'
           
         })
+        .when('/login', {
+            templateUrl: 'templates/login.html',
+            controller: 'loginCtrl'
+          
+        })
+        
         
         .when('/dashboard', {
             templateUrl: 'templates/dashboard.html',
@@ -16,25 +22,6 @@ app.config(function($routeProvider) {
         
 
         });
-       /* .when('/admin/:id', {
-        controller: 'adminDetailsCtrl',
-        controllerAs: 'vm',
-        templateUrl: 'templates/admin_registros-detalles.php'
-        });*/
-       
-});
-// create the controller and inject Angular's $scope
-analytics.controller('mainController', function($scope) {
-    // create a message to display in our view
-    $scope.message = 'Everyone come and see how good I look!';
-});
-analytics.controller('aboutController', function($scope) {
-    $scope.message = 'Look! I am an about page.';
-});
 
-analytics.filter('startFrom', function() {
-    return function(input, start) {console.log(input);
-        start = +start; //parse to int
-        return input.slice(start);
-    }
+       
 });
